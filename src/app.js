9cefb2ddf162517ff2214243ea04e559f416d78a
AndrewNgKF <andrew.ctrl.alt.del@gmail.com>
2017-05-07 23:46:45 +0800

    small change

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// @flow
+
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {View, Text} from 'react-native';
+import reducers from './reducers';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore()}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App;
